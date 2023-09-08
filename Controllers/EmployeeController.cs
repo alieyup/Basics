17f7c410c3b19cc140c99504e40a3a7ecade8190
AB129050 <AB129050@AB60013-129050>
2023-09-08 14:37:22 +0300

    ikinci dersin sonu

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,20 +1,35 @@
+using Basics.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basics.Controllers
 {
     public class EmployeeController:Controller
     {
-        public String Index()
+        public IActionResult Index1()
         {
-            return "Hello World.";
+            string message =$"Hello World!.{DateTime.Now.ToString()}";
+            return View("Index1",message);
         }
         public ViewResult Index2()
         {
-            return View("Index");
+            var names = new String[]
+            {
+                "Ali",
+                "Orçun",
+                "Ömer"
+            };
+            return View(names);
         }
         public IActionResult Index3()
         {
-            return Content("Employee");
+            var list = new List<Employee>()
+            {
+                new Employee(){Id=1,FirstName="Ali Eyüp",LastName="Doğan",Age=25},
+                new Employee(){Id=2,FirstName="Orçun",LastName="Okan",Age=30},
+                new Employee(){Id=3,FirstName="Ömer",LastName="aydoğdu",Age=20},
+                
+            };
+            return View("Index3", list);
         }
     }
 }
